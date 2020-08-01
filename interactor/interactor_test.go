c78122850ad3c5c9fc9fe04a57cdb236ac385ac5
arstevens <aleks.stevens@protonmail.com>
2020-08-01 15:25:40 -0700

    finished testing interactor. Working

--- interactor/interactor_test.go
+++ interactor/interactor_test.go
@@ -8,7 +8,40 @@ import (
 
 func TestInteractor(t *testing.T) {
   refTypes := map[processor.ReflectorCode]reflectorCreator{
-    "1":reflector.NewPlainReflector,
+    "rf1":reflector.NewPlainReflector,
   }
+  cmLoaders := map[processor.ChangeMapCode]changeMapLoader{
+    "cm1":reflector.LoadSHA1ChangeMap,
+  }
+  cmCreator := map[processor.ChangeMapCode]changeMapCreator{
+    "cm1":reflector.NewSHA1ChangeMap,
+  }
+
+  g := Generator{
+    reflectorTypes:refTypes,
+    changeMapCreators:cmCreator,
+    changeMapLoaders:cmLoaders,
+  }
+
+  cm1, err := g.OpenChangeMap("cm1", "/home/aleksandr/Workspace/cmfile")
+  if err != nil {
+    panic(err)
+  }
+
+  cm2, err := g.NewChangeMap("cm1", "/home/aleksandr/Workspace/testzone", "/home/aleksandr/Workspace/cmfile2")
+  if err != nil {
+    panic(err)
+  }
+
+  ref1, err := g.Reflect("rf1", cm1, cm2)
+  if err != nil {
+    panic(err)
+  }
+
+  err = ref1.Backup()
+  if err != nil {
+    panic(err)
+  }
+
 
 }
