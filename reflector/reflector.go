00883285ae0e8ad48c2dd5e2ecfc04b46b739d7b
arstevens <aleks.stevens@protonmail.com>
2020-08-10 13:28:32 -0700

    fixed path issue in reflector

--- reflector/reflector.go
+++ reflector/reflector.go
@@ -70,8 +70,11 @@ func handleDeletions(deletes []string, root string) error {
 func handleCreations(creates []string, reflecting processor.ChangeMap, original processor.ChangeMap) error {
   fmt.Println(creates)
   for _, creation := range creates {
-    creationPath := extendPath(reflecting.RootDir(), creation)
-    copyPath := extendPath(original.RootDir(), creation)
+    creationRoot := extendPath(reflecting.RootDir(), reflecting.RootName())
+    copyRoot := extendPath(original.RootDir(), original.RootName())
+
+    creationPath := extendPath(creationRoot, creation)
+    copyPath := extendPath(copyRoot, creation)
     fmt.Println("src:" + copyPath)
     fmt.Println("dst:" + creationPath)
 
