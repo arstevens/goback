11f9e8833e581f7639d4d9b1995116e32491d421
arstevens <aleks.stevens@protonmail.com>
2020-08-15 09:58:27 -0700

    added debug statement

--- processor/detect.go
+++ processor/detect.go
@@ -145,6 +145,7 @@ func (f *fsDetector) NextChange() (fsChange, error) {
   }
   fPath := strings.Replace(event.Name, f.keymap[chosen], "", 1)
   if strings.Contains(fPath, ".part") {
+    fmt.Println("here")
     //File not done being created
     <-time.After(time.Second)
     fPath = strings.Replace(fPath, ".part", "", 1)
